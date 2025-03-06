473377cd9f14bb97a0fca0283aae805e54c1d399
BradenEverson <bradeneverson@gmail.com>
2025-03-06 14:49:52 -0600

    Basic parse

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PoseData {
+    version: f32,
+    people: Vec<Person>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Person {
+    person_id: Vec<i32>,
+    pose_keypoints_2d: Vec<f32>,
+    face_keypoints_2d: Vec<f32>,
+    hand_left_keypoints_2d: Vec<f32>,
+    hand_right_keypoints_2d: Vec<f32>,
+    pose_keypoints_3d: Vec<f32>,
+    face_keypoints_3d: Vec<f32>,
+    hand_left_keypoints_3d: Vec<f32>,
+    hand_right_keypoints_3d: Vec<f32>,
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::{fs::File, io::Read};
+
+    use crate::PoseData;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn loading_soldering_example() {
+        let mut buf = String::new();
+        let mut file = File::open("files/soldering.json").expect("Load Soldering");
+        file.read_to_string(&mut buf).expect("Read to buf");
+
+        let pose: PoseData = serde_json::from_str(&buf).expect("Failed to parse");
+        panic!("{pose:?}")
     }
 }
