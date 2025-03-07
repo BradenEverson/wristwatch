75460df041131bbd9768d529b0ef4f6c41c1b51a
BradenEverson <bradeneverson@gmail.com>
2025-03-07 04:18:14 -0600

    Add tests to angle formula

--- src/math.rs
+++ src/math.rs
@@ -1,5 +1,7 @@
 //! Core math implemenatation for angles between vectors
 
+use serde::{Deserialize, Serialize};
+
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub struct Point(pub f32, pub f32);
 
@@ -29,5 +31,21 @@ impl LineSegment {
     pub fn vector(self) -> Vector {
         let LineSegment(Point(x1, y1), Point(x2, y2)) = self;
         Vector(x2 - x1, y2 - y1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::f32::consts::PI;
+
+    use super::{LineSegment, Point};
+
+    #[test]
+    fn angle_between_perpendicular_vectors() {
+        let la = LineSegment(Point(0.0, 0.0), Point(0.0, 1.0)).vector();
+        let lb = LineSegment(Point(0.0, 0.0), Point(1.0, 0.0)).vector();
+
+        let angle = la.angle_between(&lb);
+        assert_eq!(angle, PI / 2.0)
     }
 }
